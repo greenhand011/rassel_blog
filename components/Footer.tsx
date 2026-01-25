bf965ca94e90d4c2d434f345a9a803e0621d1930
greenhand011 <rassel_work@outlook.com>
2026-01-25 21:44:52 +0800

    add star button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+// 1. 引入按钮插件
+import GitHubButton from 'react-github-btn'
 
 export default function Footer() {
   return (
@@ -19,6 +21,22 @@ export default function Footer() {
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
           <SocialIcon kind="medium" href={siteMetadata.medium} size={6} />
         </div>
+
+        {/* 2. 新增的 Star 按钮 (开始) */}
+        <div className="mb-3 flex space-x-4">
+           <GitHubButton 
+             href="https://github.com/greenhand011/rassel_blog"
+             data-color-scheme="no-preference: light; light: light; dark: dark;" 
+             data-icon="octicon-star" 
+             data-size="large" 
+             data-show-count="true" 
+             aria-label="Star greenhand011/rassel_blog on GitHub"
+           >
+             Star
+           </GitHubButton>
+        </div>
+        {/* 新增的 Star 按钮 (结束) */}
+
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,7 +12,7 @@ const siteMetadata = {
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
+  github: 'https://github.com/greenhand011/rassel_blog',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
